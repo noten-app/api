26a29a761e3eb5caa524af7a6ed248f59c485944
Konstantin Protzen <cuzimbisonratte@gmail.com>
2023-07-19 19:55:03 +0200

    Remove because wildcard already in index

--- v1/routes.js
+++ v1/routes.js
@@ -15,7 +15,4 @@ router.use('/grades', require('./grades.js'));
 // HOMEWORK
 router.use('/homework', require('./homework.js'));
 
-// Wildcard route for /v1
-router.get('*', (req, res) => res.status(404).send('Warning: Route /v1' + req.url + ' does not exist'));
-
 module.exports = router
